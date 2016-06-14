657a9a67a88139cee9c72076eb576caff121bf4f
gabrielahingston <gabyhingston@icloud.com>
2016-06-14 15:47:33 +0800

    game upload

---
+++ game/js/index.js
@@ -0,0 +1,38 @@
+alert("Once there was a young woman, with a normal family with a normal job but eversince that day she wasnt normal...EVER SINCE THAT DREADFUL DAY");
+
+var question = prompt("You run into a pig. Do you kill it for food or do you keep it?") 
+
+if (question === "kill") {
+  alert("You are a selfish person and decided to kill a pigs valuble life!");
+}
+
+else {
+  alert("You are a kind person and decided to keep the poor little pig!");
+}
+
+var question2 = prompt ("you are carrying the little pig in your arms and you hear a creek behind you. Do you let the pig run free and turn around and see what the noise was or do you keep the pig and see what the nois was together?")
+if(question2=== "let the pig free"){
+alert("you are nice to let the pig free and out of danger but what if the sound wasnt a monster and then the pigged died so you arent thinking hard");
+ }
+
+else{
+  alert("you are clever to keep the pig and keep it out of getting hurt in the dark!");
+}
+
+var question3 = prompt("the noise was just a creek and you decide to walk onwards but then you find out a zombie is right next to you and you see a sword in a cave? do you grab the sword or run away with the pig?")
+
+if(question3=== "run away with the pig")
+  {alert("you are a scardy cat and run away but the zombie follows you and grabs the pig and eats it...");
+  }
+else{
+  alert("you grab the sword and kill the zombie and you quietly run away from the zombie"); }
+  
+var question4 = prompt("you are now safe but in the morning you find that the pigs bones were on the floor, do you follow the pigs bones or do you not care?")
+
+  if(question4 === "follow the pigs bone") { 
+    alert("you are nice to look who took the pig but it wasted your time. GAME OVER!"); 
+  }
+  else{
+   alert("you are a cruel person! GAME OVER!"); 
+  }
+alert("Thank you for playing my game! I really hope you like it! If you like my game please tell your friends about my game and please ask to play my game! Thanks !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1")
